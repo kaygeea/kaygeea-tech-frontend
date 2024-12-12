8946e7a69984f21d6fafeb36eff7614107e5b41f
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2024-12-12 10:36:40 +0100

    Add project overview component files

---
+++ src/app/projects-overview/projects-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects-overview.component.html',
+  styleUrl: './projects-overview.component.css'
+})
+export class ProjectsOverviewComponent {
+
+}
