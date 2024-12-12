caa5cf23e2c269133f9fd857fd369b79bc7d0f93
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2024-12-12 10:37:40 +0100

    Add project detail service files

---
+++ src/app/services/project-detail.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Project } from '../interfaces/project';
+import { ProjectDetail, ProjectDetailContent } from '../interfaces/project-details';
+import { Observable, mergeMap, map, toArray, firstValueFrom } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectDetailService {
+  protected pd_url = 'http://localhost:3000/projects/';
+
+  constructor(private http: HttpClient) { }
+
+  fetchProjectDetails(projectName: string | null, projectDetailId: string | null) : Observable<ProjectDetail> {  
+    return this.http.get<ProjectDetail>(this.pd_url + projectName + '/' + projectDetailId);
+  }
+
+  populateProject(projectName: string | null, projects: Project[]) : Project {
+    try {
+      if (projects && projectName) {
+        const foundProject = projects.find(project => project.name === projectName);
+  
+        if (!foundProject) {
+          throw new Error(`Project with name "${projectName}" not found.`);
+        }
+        return foundProject;
+      } else {
+        throw new Error(`Incomplete number of arguments. You did not provide "${ projectName === null ? "project name" : "projects" }"`);
+      }
+    } catch (err) {
+      console.error(err);
+      throw new Error("Incomplete arguments", { cause: err });
+    } 
+  }
+}
