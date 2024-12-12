744738d4f54166ae58f32108846a567ff9ef5ab9
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2024-12-12 10:40:53 +0100

    Add from-kebab pipe files

---
+++ src/app/utils/pipes/from-kebab.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FromKebabPipe } from './from-kebab.pipe';
+
+describe('FromKebabPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FromKebabPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
