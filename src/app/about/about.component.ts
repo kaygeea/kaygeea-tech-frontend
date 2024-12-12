ab7bde6cd058cc94b868b70ba52fc2c9ae8373cc
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2024-12-12 10:30:36 +0100

    add about component files

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Profile } from '../interfaces/profile';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent implements OnInit{
+  @Input({required: true}) profile!: Profile;
+
+  ngOnInit(): void {}
+}
