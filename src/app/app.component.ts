1675166398a5b4f2a5b219d703cc95161bc2e453
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2024-12-12 10:30:13 +0100

    add app component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileService } from './services/profile.service';
+import { Profile } from './interfaces/profile';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'kaygeea-tech';
+export class AppComponent implements OnInit{
+  public profile!: Profile;
+
+  constructor(private ProfileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.ProfileService.fetchHomeProfile().subscribe(profileData => {
+      this.profile = profileData;
+    })
+  }
+
+  assignData(component: any) {
+    if (component instanceof HomeComponent) {
+      component.homeProfile = this.profile;
+    } else if (component instanceof ProjectsComponent) {
+      component.profile = this.profile;
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,64 @@
-import { Routes } from '@angular/router';
+import { Routes, ExtraOptions, RouterFeature } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectsOverviewComponent } from './projects-overview/projects-overview.component';
 
-export const routes: Routes = [];
+// const resolvedNameTitle: ResolveFn<string> = () => Promise.resolve(inject(ProjectDetailService));
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        title: 'Full Stack Web Developer & SE',
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        title: 'About',
+        path: 'about',
+        component: HomeComponent,
+    },
+    {
+        title: 'Skills',
+        path: 'skills',
+        component: HomeComponent,
+    },
+    {
+        // title: 'Projects',
+        path: 'home-projects',
+        component: HomeComponent,
+    },
+    {
+        title: 'Projects',
+        path: 'projects',
+        component: ProjectsComponent,
+        children: [
+            {
+                path: '',
+                component: ProjectsOverviewComponent
+            },
+            {
+                path: ':projectName/:projectScope',
+                component: ProjectDetailComponent
+            },
+        ]
+    },
+    {
+        title: 'Contact',
+        path: 'contact',
+        component: HomeComponent,
+    },
+    {
+        title: 'Education',
+        path: 'education',
+        component: HomeComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    }
+];
